edf26f946f5fe3ead06d1c0bc77a08859891c97d
Nicholas Car <nicholas.car@csiro.au>
2016-07-19 22:18:26 +1000

    minor

--- overwrite.sh
+++ overwrite.sh
@@ -1,5 +1,11 @@
 PROMS_DIR=$1
+WEB_SUBFOLDER=$2
+
+sed 's/url('\/static/url('\/$2/static/' static/css/ga_theme.css
 
 cp templates/* $PROMS_DIR/templates/
 cp static/css/* $PROMS_DIR/static/css/
 cp static/img/* $PROMS_DIR/static/img/
+
+
+
