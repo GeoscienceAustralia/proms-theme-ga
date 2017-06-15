b03d621fa4c09b033ac8ff2a5555821cdd6d0546
Laura Stanford <laura.stanford@ga.gov.au>
2017-06-15 14:51:21 +1000

    Updating ga-theme to align with current PROMS version

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ GA Theming files for PROMS Server (Python Flask templates and CSS)
 
 ### How to use
 1. clone to the target server
-2. >sh overwrite.sh PATH_TO_PROMS_INSTALL WEB_SUBFOLDER
+1. `sh overwrite.sh PATH_TO_PROMS_INSTALL WEB_SUBFOLDER`
+   e.g. `sudo sh overwrite.sh /var/www/proms /var/www/proms/templates`
 
-It just overwrites the template/ and static/ files in PROMS Server with these here and includes the WEB_SUBFOLDER in the CSS file URLs. 
+It overwrites the template/ and static/ files in PROMS Server with these here and includes the WEB_SUBFOLDER in the CSS file URLs. 
 
-Nick
+
--- overwrite.sh
+++ overwrite.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
 PROMS_DIR=$1
 WEB_SUBFOLDER=$2
 
-sed 's/url('\/static/url('\/$2/static/' static/css/ga_theme.css
+sudo cp  ${WEB_SUBFOLDER}/template_layout_vanilla.html ${WEB_SUBFOLDER}/template_layout_ga.html
+sudo sed -ie 's/vanilla_theme/ga_theme/g' ${WEB_SUBFOLDER}/template_layout_ga.html
+sudo sed -ie 's/template_header_vanilla/ga_header/' ${WEB_SUBFOLDER}/template_layout_ga.html
+sudo sed -ie 's/template_footer_vanilla/ga_footer/' ${WEB_SUBFOLDER}/template_layout_ga.html
+sudo sed -ie 's/template_layout_vanilla/template_layout_ga/' ${WEB_SUBFOLDER}/*.html
 
 cp templates/* $PROMS_DIR/templates/
 cp static/css/* $PROMS_DIR/static/css/
